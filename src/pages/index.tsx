50fa6ce7a5820675a1664e86cb9774cc49fcf43c
gokhancaliskan <caliskancompany@gmail.com>
2024-05-14 20:50:08 +0300

    chore: Update API endpoint URL to fix broken fetch request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage<HomeProps> = ({ posts }) => {
 export const getServerSideProps: GetServerSideProps =
   async () => {
     const res = await fetch(
-      "https://coffeefairytale.vercel.app/api/posts"
+      "https://coffeefairytale-git-main-gokhancaliskans-projects.vercel.app//api/posts"
     );
     const posts = await res.json();
     return {
